d2ea1bbc44af25a910c4a7da4ac26acd406188ae
Carlos Vesta <vesta.carlos@gmail.com>
2025-10-18 06:17:46 +0200

    09.tabuada

---
+++ 1.curso_em_video/modulo_1/09.tabuada.py
@@ -0,0 +1,29 @@
+# 9 - Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+numero = int(input('Digite um número inteiro, e veja a sua tabuada: '))
+
+a = numero * 1
+b = numero * 2
+c = numero * 3
+d = numero * 4
+e = numero * 5
+f = numero * 6
+g = numero * 7
+h = numero * 8
+i = numero * 9
+j = numero * 10
+k = numero * 11
+l = numero * 12
+
+print('{} * 1 = {}'.format(numero, a))
+print('{} * 2 = {}'.format(numero, b))
+print('{} * 3 = {}'.format(numero, c))
+print('{} * 4 = {}'.format(numero, d))
+print('{} * 5 = {}'.format(numero, e))
+print('{} * 6 = {}'.format(numero, f))
+print('{} * 7 = {}'.format(numero, g))
+print('{} * 8 = {}'.format(numero, h))
+print('{} * 9 = {}'.format(numero, i))
+print('{} * 10 = {}'.format(numero, j))
+print('{} * 11 = {}'.format(numero, k))
+print('{} * 12 = {}'.format(numero, l))
