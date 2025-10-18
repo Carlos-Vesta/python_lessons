f789445de2e74e617f35068777f0dab25207362c
Carlos Vesta <vesta.carlos@gmail.com>
2025-10-18 07:21:02 +0200

    12.preco_desconto

---
+++ 1.curso_em_video/modulo_1/12.preco_desconto.py
@@ -0,0 +1,10 @@
+# 12 - Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de
+# desconto.
+
+preco_produto = float(input('Digite o preço do produto: '))
+
+desconto = preco_produto * 0.05
+preco_desconto = preco_produto - desconto
+
+print('O produto tem como preço {:,.2f} meticais, e com desconto de 5% o ' \
+'preço passa a ser {:,.2f} meticais.'.format(preco_produto, preco_desconto))
